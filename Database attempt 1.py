8276b5c50d346f24d537c6c3a6e71f462741d6ce
judecco <[jude.onwuemezia@gmail.com]>
2021-11-04 12:50:28 +0000

    updated save

--- Database attempt 1.py
+++ Database attempt 1.py
@@ -1,89 +1,103 @@
-from flask import Flask, render_template, request, redirect
-import mysql.connector
-
-db = mysql.connector.connect( host="localhost", user="root", password="root", database="manchester")
-
-cursor = db.cursor()
-
-
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, StringField, SelectField, SubmitField
 
 application=Flask(__name__)
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+application.config['SECRET_KEY'] = getenv('secretkey')
+
+db = SQLAlchemy(application)
+
+db.drop_all()
+db.create_all()
+
+
+class Employee(db.Model):
+    regno = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50))
+    salary = db.Column(db.Integer)
+    marks = db.Column(db.Integer)
+    subject = db.Column(db.String(50))
+    dept = db.Column(db.String(50))
+
+db.drop_all()
+db.create_all()
+
+class AddEmp(FlaskForm):
+    emp_name = StringField("Name")
+    salary = IntegerField("Salary")
+    marks = IntegerField("Marks")
+    subject = SelectField("Subject", choices=[('python', 'Python'), ('java', 'Java'), ('php', 'PHP'), ('sql', 'SQL')])
+    department = SelectField('Department', choices=[('IT', 'Information technology'), ('HR', 'Human Resources'), ('sales', 'Sales'), ('training', 'Training')])
+    submit = SubmitField('Add Employee')
+
+class UpdateEmp(FlaskForm):
+    emp_name = StringField("Name")
+    salary = IntegerField("Salary")
+    marks = IntegerField("Marks")
+    subject = SelectField("Subject", choices=[('python', 'Python'), ('java', 'Java'), ('php', 'PHP'), ('sql', 'SQL')])
+    department = SelectField('Department', choices=[('IT', 'Information technology'), ('HR', 'Human Resources'), ('sales', 'Sales'), ('training', 'Training')])
+    submit = SubmitField('Update Employee')
+
 
 @application.route("/")
 def homePage():
-    cursor.execute("Select * from consultants")
-    data=cursor.fetchall()
-    return render_template("Homepage.html", records=data)
-
-@application.route("/saveEditedForm", methods=["POST"])
-def saveEditedForm():
-    regno=request.form["regno"]
-    name=request.form["na"]
-    department=request.form["dept"]
-    salary=request.form["sal"]
-    subject=request.form["subject"]
-    marks=request.form["marks"]
-
-    sqlquery="update consultants set name='{0}', salary={1}, marks={2}, subject='{4}', dept='{5}' where regno={3}".format(name, salary, marks, regno, subject, department)
-    cursor.execute(sqlquery)
-    db.commit()
-    return redirect("/")
+    emps = Employee.query.all()
+    return render_template('homepage.html', records=emps)
 
 
-@application.route("/editRecordForm/<regno>")
+@application.route('/editRecord/<int:regno>', methods=['GET', 'POST'])
 def editRecordForm(regno):
-    cursor.execute("Select * from consultants where regno={0}".format(regno))
-    data=cursor.fetchone()
-    return render_template("EditForm.html", record=data)
+    form = UpdateEmp()
+    emp = Employee.query.filter_by(regno=regno).first()
+    if request.method == 'POST':
+        emp.name = form.emp_name.data
+        emp.salary = form.salary.data
+        emp.dept = form.department.data
+        emp.subject = form.subject.data
+        emp.marks = form.marks.data
+        db.session.commit()
+        return redirect("/")
+    return render_template('EditForm.html', form=form)
 
-@application.route("/filterrecords", methods=["POST"])
+@application.route("/filterrecords",methods=["POST"])
 def filterrecords():
     if request.form["dept"]=="all":
         return redirect("/")
     else:
-        cursor.execute("Select * from consultants where dept='{0}'".format(request.form["dept"]))
-        data=cursor.fetchall()
-        return render_template("Homepage.html", records=data)
-
-@application.route("/filtersubject", methods=["POST"])
-def filtersubject():
-    if request.form["subject"]=="all":
-        return redirect("/")
-    else:
-        cursor.execute("Select * from consultants where subject='{0}'".format(request.form["subject"]))
-        data=cursor.fetchall()
-        return render_template("Homepage.html", records=data)
-
-
-@application.route("/addnewRecord")
-def addNewRecord():
-    return render_template("input.html")
+        data = Employee.query.filter_by(dept=request.form["dept"]).all()
+        return render_template("Homepage.html",records=data)
     
 
-@application.route("/saveRecord", methods=["POST"])
+@application.route("/saveRecord", methods=["GET", "POST"])
 def saveRecord():
-    cursor.execute("select ifnull(max(regno),0)+1 from consultants")
-    newregno=cursor.fetchone()
-    name=request.form["na"]
-    department=request.form["dept"]
-    salary=request.form["sal"]
-    subject=request.form["subject"]
-    marks=request.form["marks"]
-    client=request.form["client"]
-    sqlquery= "insert into consultants values({0}, '{1}', '{2}', {3}, '{4}', {5}, '{6}')".format(newregno[0], name, department, salary, subject, marks, client)
-    cursor.execute(sqlquery)
-    db.commit()
-    return redirect("/")
+    form = AddEmp()
+    if request.method == 'POST':
+        name=form.emp_name.data
+        department=form.department.data
+        salary=form.salary.data
+        subject=form.subject.data
+        marks=form.marks.data
+        newemp = Employee(name=name, dept=department, salary=salary, subject=subject, marks=marks)
+        db.session.add(newemp)
+        db.session.commit()
+        return redirect("/")
+    return render_template("input.html", form=form)
+    
 
-@application.route("/deleteEmployee/<regno>")
+@application.route("/deleteEmployee/<int:regno>")
 def deleteEmployee(regno):
-    cursor.execute("delete from consultants where regno={0}".format(regno))
+    emp = Employee.query.filter_by(regno=regno).first()
+    db.session.delete(emp)
+    db.session.commit()
     return redirect("/")
 
-@application.route("/personaldetails/<regno>")
+@application.route("/personaldetails/<int:regno>")
 def personalInformation(regno):
-    cursor.execute("Select * from consultants where regno={0}".format(regno))
-    data=cursor.fetchone()
-    return render_template("personalInformation.html", record=data)
+	data = Employee.query.filter_by(regno=regno).first()
+	return render_template("personalinforamtion.html",record=data)
 
-application.run(debug=True)
+application.run(debug=True, host = "0.0.0.0")
